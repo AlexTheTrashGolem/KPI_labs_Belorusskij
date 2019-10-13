620cde02723efa7698d94f17b1763c5c52f7f3b9
AlexTheTrashGolem <a.belorusskij@gmail.com>
2019-10-13 20:21:26 +0300

    Лабы

---
+++ OP_laba2.py
@@ -0,0 +1,5 @@
+
+x=float(input("X="))
+y=float(input("Y="))
+answer = (x*y<=0 and y-x-1>=0)
+print(answer)
---
+++ OP_laba3.py
@@ -0,0 +1,12 @@
+import math
+x=float(input())
+if x>4 or x<0:
+    raise Exception('x has to be in [0;4]')
+counter=1
+s=0
+s_current=1
+while s_current>=0.000001 or s_current<=-0.000001:
+    s+=s_current
+    s_current=(-1)**counter*x**(2*counter)/math.factorial(2*counter)
+    counter+=1
+print(s)
